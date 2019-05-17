79a6f4227713670240f1a438cbed360b2707e1ec
Mark Olson <mdo@ieee.org>
2019-05-17 12:02:01 -0700

    set size of columns in list control

--- PyMarkMedia/MarkMedia.py
+++ PyMarkMedia/MarkMedia.py
@@ -185,6 +185,8 @@ class MainFrame ( wx.Frame ):
 # start copying here
         self.m_listCtrlVidComments.InsertColumn(0, 'MagicNum')
         self.m_listCtrlVidComments.InsertColumn(1, 'Comment')
+        self.m_listCtrlVidComments.SetColumnWidth(0, 50)
+        self.m_listCtrlVidComments.SetColumnWidth(1, 1500)
 
 
         bSizerPanel.Add( self.m_listCtrlVidComments, 0, wx.ALL|wx.EXPAND, 5 )
--- PyMarkMedia/_01_listCtrlInit.py
+++ PyMarkMedia/_01_listCtrlInit.py
@@ -2,4 +2,6 @@
 # start copying here
         self.m_listCtrlVidComments.InsertColumn(0, 'MagicNum')
         self.m_listCtrlVidComments.InsertColumn(1, 'Comment')
+        self.m_listCtrlVidComments.SetColumnWidth(0, 50)
+        self.m_listCtrlVidComments.SetColumnWidth(1, 1500)
 

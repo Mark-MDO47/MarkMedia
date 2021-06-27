1e4381e5085bd40cce307c39b372258c3dd6094e
Mark Olson <mdo@ieee.org>
2021-06-26 20:47:32 -0700

    move wxFormBuilder files to old directory

--- PyMarkMedia/_01_listCtrlInit.py
+++
@@ -1,7 +0,0 @@
-#
-# start copying here
-        self.m_listCtrlVidComments.InsertColumn(0, 'MagicNum')
-        self.m_listCtrlVidComments.InsertColumn(1, 'Comment')
-        self.m_listCtrlVidComments.SetColumnWidth(0, 50)
-        self.m_listCtrlVidComments.SetColumnWidth(1, 1500)
-
